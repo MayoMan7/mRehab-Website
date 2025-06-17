88299b9af2d9b9f9e5688136bb14853907f9d061
Rohin Kumar <76756199+MayoMan7@users.noreply.github.com>
2025-06-17 12:10:30 -0400

    basic techonnolgies pages done

--- mrehab/src/app/router.tsx
+++ mrehab/src/app/router.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from '../components/Header';
+import TechnologiesMobileApp from './pages/technologies-mobileapp';
+import Technologies3DPrinting from './pages/Technologies3DPrinting';
+
 
 import Landing from './pages/landing';
 const AppRouter: React.FC = () => (
     <Router>
         <Routes>
             <Route path="/" element={<Landing />} />
+            <Route path="/technologies/mobileapp" element={<TechnologiesMobileApp />} />
+            <Route path="/technologies/3Dprinting" element={<Technologies3DPrinting />} />
+
         </Routes>
     </Router>
 );
